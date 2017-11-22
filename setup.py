868faef2fefa469580d876f450ce27ef85c20b18
Demo User <debian@beaglebone.localdomain>
2017-11-22 09:36:40 +0000

    Altered for installation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import versioneer
 
 setup(
-    name='qem',
+    name="qem",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     description='QEM Backplane plugin for ODIN framework',
