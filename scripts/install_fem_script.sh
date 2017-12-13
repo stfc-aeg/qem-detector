2308e4b82cfd00f0559a6faef776b5271adf630f
Benjamin Edwards <32669118+BenCEdwards@users.noreply.github.com>
2017-12-13 12:02:56 +0000

    Update install_fem_script.sh

--- scripts/install_fem_script.sh
+++ scripts/install_fem_script.sh
@@ -7,7 +7,9 @@ virtualenv --system-site-packages venv2.7
 source venv2.7/bin/activate
 
 #Install the adapter and server
+git config --global http.sslVerify false
 git clone https://github.com/BenCEdwards/odin-control
+git config --global http.sslVerify true
 cd odin-qem
 python fem_setup.py install
 cd ../odin-control
