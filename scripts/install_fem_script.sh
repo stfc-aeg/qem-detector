79d4ee42dbc0be055a184bb8a02076cc7dbae654
root <Benjamin.Edwards@stfc.ac.uk>
2017-12-13 11:54:01 +0000

    Added installation script for femm ii ::# Please enter the commit message for your changes. Lines starting

---
+++ scripts/install_fem_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Setup the Odin Server with QEM adapter to run as a service
+
+#Switch to a virtual environment
+su -
+virtualenv --system-site-packages venv2.7
+source venv2.7/bin/activate
+
+#Install the adapter and server
+git clone https://github.com/BenCEdwards/odin-control
+cd odin-qem
+python fem_setup.py install
+cd ../odin-control
+python setup.py install
+
+#Setup the script to run the server
+cd ..
+mkdir bin
+cp odin-qem/scripts/QEM.sh bin
