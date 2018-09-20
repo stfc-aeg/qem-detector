7fab5960fe9a964c06b469508b79c4313aa8eed9
Sophie Kirkham <sophie.kirkham@stfc.ac.uk>
2018-09-20 13:52:49 +0100

    Amended host setup file to pull correct version of odin-control

--- control/host-adapter/setup.py
+++ control/host-adapter/setup.py
@@ -6,4 +6,7 @@ setup(
     author='Ben Edwards',
     author_email='benjamin.edwards@stfc.ac.uk',
     packages=find_packages(),
+    install_requires=['odin==0.2.0'],
+    dependency_links = ['https://github.com/stfc-aeg/odin-control/zipball/0.2.0#egg=odin-0.2.0'],
+
 )
